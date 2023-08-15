58ec281ed527db13e84e41b4e7b7ca5a40c00b86
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2023-08-15 18:18:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 2172 ms, Memory: 146724 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Member> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        Member[] members = new Member[n];
+        
         for (int i = 0; i < n; i++) {
-            Member member = new Member();
-            member.age = scanner.nextInt();
-            member.name = scanner.next();
-            list.add(member);
+            members[i] = new Member();
+            members[i].age = scanner.nextInt();
+            members[i].name = scanner.next();
         }
 
-        Collections.sort(list, new Comparator<Member>() {
+        Arrays.sort(members, new Comparator<Member>() {
             @Override
             public int compare(Member o1, Member o2) {
                 return o1.age - o2.age;
             }
         });
-
-        for (int i = 0; i < list.size(); i++) {
-            Member member = list.get(i);
-            System.out.println(member.age + " " + member.name);
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println(members[i].age + " " + members[i].name);
         }
 
     }
