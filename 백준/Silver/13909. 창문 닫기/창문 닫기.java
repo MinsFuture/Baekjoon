4a272832f42276054c446a65cf95fbe5ee6dcd2e
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2023-08-31 20:51:42 +0900

    [Silver V] Title: 창문 닫기, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int i;
+        for (i = 1; i <= Math.sqrt(n); i++) {
+        }
+
+        System.out.println(i-1);
+    }
+}
