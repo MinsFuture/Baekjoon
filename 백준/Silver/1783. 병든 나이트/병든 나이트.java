2e9abbe30107ae525754220657aed337ac7fca1e
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2023-09-13 20:28:55 +0900

    [Silver III] Title: 병든 나이트, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/1783. 병든 나이트/병든 나이트.java
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.java
@@ -9,13 +9,10 @@ public class Main {
         if (n == 1) {
             System.out.println(1);
         } else if (n == 2) {
-            if (1 <= m && m <= 2) {
-                System.out.println(1);
-            } else if (3 <= m && m <= 4) {
-                System.out.println(2);
-            } else if (5 <= m && m <= 6) {
-                System.out.println(3);
-            } else {
+            if (1 <= m && m <= 6) {
+                System.out.println((m+1)/2);
+            }
+             else {
                 System.out.println(4);
             }
         } else {
