20327e7c255870855fed211b35a16d0ac10c850d
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2024-01-12 23:25:37 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1184 ms, Memory: 92948 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] check;
+    static int count;
+    static ArrayList<ArrayList<Integer>> graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int vertex = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        int startVertex = Integer.parseInt(st.nextToken());
+
+        check = new int[vertex+1];
+        graph = new ArrayList<>();
+
+        for (int i = 0; i < vertex + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int fromVertex = Integer.parseInt(st.nextToken());
+            int toVertex = Integer.parseInt(st.nextToken());
+
+            graph.get(fromVertex).add(toVertex);
+            graph.get(toVertex).add(fromVertex);
+        }
+
+        for (int i = 1; i <graph.size(); i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        count = 1;
+        dfs(startVertex);
+
+        for (int i = 1; i < check.length; i++) {
+            bw.write(check[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int vertex) {
+        check[vertex] = count;
+
+        for (int i = 0; i < graph.get(vertex).size(); i++) {
+            int newVertex = graph.get(vertex).get(i);
+
+            if (check[newVertex] == 0) {
+                count++;
+                dfs(newVertex);
+            }
+        }
+
+
+    }
+}
