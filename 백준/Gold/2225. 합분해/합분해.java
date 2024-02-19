6b1b2f0c10e28b8f8515b14c18bfce3f5f4866e9
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2024-02-19 20:20:06 +0900

    [Gold V] Title: 합분해, Time: 216 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    // TODO Auto-generated method stub
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        long[] dp = new long[n+1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = 1;
-        }
-
-        for (int i = 0; i < k-1; i++) {
-            long[] newDp = new long[n+1];
-            for (int j = 1; j <= n; j++) {
-                for (int index = j; index >= 0; index--) {
-                   newDp[j] += dp[index];
-                }
-            }
-
-            for (int num = 1; num <= n; num++) {
-                dp[num] = newDp[num] % 1000000000;
-            }
-        }
-
-        System.out.println(dp[n] % 1000000000);
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt(); // n 입력받음
+		int k = s.nextInt(); // k 입력받음
+		
+		int[][] dp = new int[201][201]; // k번 더해서 n이 되는 경우의 수
+		
+		for(int i=1;i<=k;i++) {
+			dp[i][0]=1;
+		}
+		for(int i=1;i<=k;i++) {
+			for(int j=1;j<=n;j++) {
+				dp[i][j] = (dp[i][j-1] + dp[i-1][j])%1000000000; // 1000000000으로 나누는 걸 출력할 때 주면 틀렸다고 뜸.
+			}
+		}
+		System.out.println(dp[k][n]);
     }
 }
