e71a5acacd528e013b76c37e4c0105de7d474f99
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2024-04-01 14:31:40 +0900

    [level 2] Title: 귤 고르기, Time: 92.10 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        tangerine = Arrays.stream(tangerine).distinct().toArray();
+        
+        for(int i = 0; i<tangerine.length; i++){
+            list.add(map.get(tangerine[i]));
+        }
+        
+        Collections.sort(list, Comparator.reverseOrder());
+        // 4 3 1 , 2 2 2 1 1 
+        
+        int index = 0;
+        
+        while(k > 0){
+            k -= list.get(index++);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
