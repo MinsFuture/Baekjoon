e217cbdf6e799efd2ebbe1eecc3c1ec35c9dd9c4
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2024-10-04 19:50:33 +0900

    [Silver II] Title: 나무 자르기, Time: 1620 ms, Memory: 321524 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] tree = new int[n];
+        int max = 0;
+
+        for(int i = 0; i<n; i++){
+            tree[i] = sc.nextInt();
+            if(max < tree[i]){
+                max = tree[i];
+            }
+        }
+
+        long result = binarySearch(tree, max, m);
+        System.out.println(result);
+    }
+
+    public static long binarySearch(int[] tree, int max, int m){
+        long low = 0;
+        long high = max;
+
+        long answer = 0;
+        long mid = (low + high) / 2;
+
+        // answer 이 m 보다 작다 -> mid가 너무 높다 -> high를 줄임
+        // answer 이 m 보다 크다 -> mid가 너무 낮다 -> low를 올림
+        while(low <= high){
+            mid = (low + high) / 2;
+            answer = 0;
+
+            for(int i = 0; i<tree.length; i++){
+                long length = tree[i] - mid;
+                if(length < 0){
+                    continue;
+                }
+                answer += length;
+            }
+            if(answer == m || mid == low || mid == high){
+                return mid;
+            }
+
+            if(answer < m){
+                high = mid;
+            }
+
+            if(answer > m){
+                low = mid;
+            }
+        }
+        return mid;
+    }
+}
