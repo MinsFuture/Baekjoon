f2217e8c87f30163f3f869f6b7a9e6c6fac28e16
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2024-12-03 16:06:47 +0900

    [Silver III] Title: 2×n 타일링, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[] dp = new int[1001];
+
         dp[1] = 1;
         dp[2] = 2;
+        dp[3] = 3;
 
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i -2]) % 10007;
+        for(int i = 4; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
-
-        System.out.println(dp[n]);
+        System.out.print(dp[n]);
     }
 }
