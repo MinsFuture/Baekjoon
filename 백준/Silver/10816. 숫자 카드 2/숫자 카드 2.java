70131b992b61d3867e0089c311e8512117651aaa
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-01-09 16:14:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1052 ms, Memory: 188648 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,36 +1,48 @@
-import java.io.*;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (!map.containsKey(num)) {
-                map.put(num, 1);
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        int []ary = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            ary[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i<n; i++){
+            if(map.get(ary[i]) != null){
+               map.put(ary[i], map.get(ary[i]) + 1);
             } else {
-                int value = map.get(num);
-                map.put(num, ++value);
+                map.put(ary[i], 1);
             }
         }
 
         int m = Integer.parseInt(br.readLine());
+        int []key = new int[m];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            if (map.containsKey(key)) {
-                bw.write(map.get(key) + " ");
-            } else {
-                bw.write(0 + " ");
-            }
+        for(int i = 0; i<m; i++){
+            key[i] = Integer.parseInt(st.nextToken());
         }
 
+        for(int i = 0; i<m; i++){
+            if(map.get(key[i]) == null){
+               bw.write(0 + " ");
+               continue;
+            }
+            bw.write(map.get(key[i]) + " ");
+        }
         bw.flush();
         bw.close();
     }
