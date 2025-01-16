bf9d3bf0da1df23eda33f45752fd8a6457477a6d
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-01-16 17:35:20 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 126528 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,84 +1,88 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Point> q = new LinkedList<>();
 
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+    static int n;
+    static int m;
+    static int []di = {-1, 0, 0, 1};
+    static int []dj = {0, -1, 1, 0};
+    static boolean[][] visited;
+    static int[][] result;
+    static Queue<Node> q;
 
-        int[][] ary = new int[n][m];
-        int[][] depth = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        int [][]ary = new int[n][m];
+
+        visited = new boolean[n][m];
+        result = new int[n][m];
+        q = new LinkedList<>();
 
         for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j<m; j++){
-                ary[i][j] = sc.nextInt();
+                ary[i][j] = Integer.parseInt(st.nextToken());
                 if(ary[i][j] == -1){
-                    depth[i][j] = -1;
-                } else if(ary[i][j] == 1){
-                    depth[i][j] = 1;
-                    q.add(new Point(i, j));
+                    visited[i][j] = true;
+                }
+
+                if(ary[i][j] == 1){
+                    q.add(new Node(i, j, 0));
                     visited[i][j] = true;
                 }
             }
         }
 
-        bfs(visited, depth, q, n, m);
+        bfs();
         int answer = 0;
 
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++) {
-                if(depth[i][j] == 0){
-                    System.out.println(-1);
+            for(int j = 0; j<m; j++){
+                if(!visited[i][j]){
+                    System.out.print(-1);
                     return;
                 }
 
-                if(depth[i][j] > answer){
-                    answer = depth[i][j] - 1;
-                }
+                answer = Math.max(answer, result[i][j]);
             }
         }
-        System.out.println(answer);
+        System.out.print(answer);
     }
 
-    public static void bfs(boolean[][] visited, int[][] depth, Queue<Point> q, int n, int m){
-        int []di = {-1, 0, 0, 1};
-        int []dj = {0, -1, 1, 0};
-
+    public static void bfs(){
         while(!q.isEmpty()){
-            Point point = q.poll();
-            int i = point.i;
-            int j = point.j;
+            Node node = q.poll();
+            result[node.i][node.j] = node.cnt;
+            
+            for(int i = 0; i<4; i++){
+                int moveI = node.i + di[i];
+                int moveJ = node.j + dj[i];
 
-            visited[i][j] = true;
-
-            for(int k = 0; k<4; k++){
-                int moveI = i + di[k];
-                int moveJ = j + dj[k];
-
-                if((0 <= moveI) && (moveI < n) && (0 <= moveJ) && (moveJ < m)
-                && depth[moveI][moveJ] != -1 && !visited[moveI][moveJ]){
-                    q.add(new Point(moveI, moveJ));
-                    depth[moveI][moveJ] = depth[i][j] + 1;
+                if(0 <= moveI && moveI < n && 0 <= moveJ && moveJ < m && !visited[moveI][moveJ]){
+                    q.add(new Node(moveI, moveJ, node.cnt + 1));
                     visited[moveI][moveJ] = true;
                 }
             }
         }
-
-
     }
 
-    public static class Point{
-        public int i;
-        public int j;
+    static class Node{
+        int i;
+        int j;
+        int cnt;
 
-        Point(int i, int j){
+        Node(int i, int j , int cnt){
             this.i = i;
             this.j = j;
+            this.cnt = cnt;
         }
     }
 
