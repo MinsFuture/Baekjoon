a54988abd3c93089622ae59a2fa36857abfdf7c5
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-01-17 17:58:43 +0900

    [Gold V] Title: 치킨 배달, Time: 216 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,19 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     static int n;
     static int m;
+    static int di[] = {-1, 0, 0, 1};
+    static int dj[] = {0, -1, 1, 0};
     static int[][] ary;
-    static List<Point> house;
-    static List<Point> chicken;
-    static boolean[] visited;
-    static int result;
+    static List<Node> chickens;
+    static List<Node> houses;
+    static boolean[] chickenVisited;
+    static int answer;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,68 +20,67 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         ary = new int[n][n];
-        house = new ArrayList<>();
-        chicken = new ArrayList<>();
-        result = Integer.MAX_VALUE;
+        chickens = new ArrayList<>();
+        houses = new ArrayList<>();
+        answer = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 ary[i][j] = Integer.parseInt(st.nextToken());
-                if (ary[i][j] == 1) {
-                    house.add(new Point(i, j));
+                if(ary[i][j] == 1){
+                    houses.add(new Node(i, j));
                 }
-                if (ary[i][j] == 2) {
-                    chicken.add(new Point(i, j));
+
+                if(ary[i][j] == 2){
+                    chickens.add(new Node(i, j));
                 }
             }
         }
-
-        visited = new boolean[chicken.size()];
+        chickenVisited = new boolean[chickens.size()];
         dfs(0, 0);
-        System.out.println(result);
+        System.out.println(answer);
     }
 
-    public static void dfs(int depth, int idx) {
-        if (depth == m) {
-            int LocalMin = 0;
+    public static void dfs(int cnt, int idx){
+        if(cnt == m){
+            int oneCombinationMin = 0;
 
-            for(int i = 0; i<house.size(); i++){
-                Point point = house.get(i);
-                int h1 = point.x;
-                int h2 = point.y;
+            for(int i = 0; i<houses.size(); i++){
                 int min = Integer.MAX_VALUE;
+                Node node = houses.get(i);
 
-                for(int j = 0; j<chicken.size(); j++){
-                    if(visited[j]) {
-                        int c1 = chicken.get(j).x;
-                        int c2 = chicken.get(j).y;
-                        int distance = Math.abs(h1 - c1) + Math.abs(h2 - c2);
-                        min = Math.min(min, distance);
+                for(int j = 0; j<chickens.size(); j++){
+                    if(chickenVisited[j]){
+                        int distance = Math.abs(node.i - chickens.get(j).i) + Math.abs(node.j - chickens.get(j).j);
+                        min = Math.min(distance, min);
                     }
                 }
-                LocalMin += min;
+                oneCombinationMin += min;
             }
-            result = Math.min(result, LocalMin);
+            answer = Math.min(oneCombinationMin, answer);
+            return;
         }
 
-        for (int i = idx; i < chicken.size(); i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                dfs(depth + 1, i + 1);
-                visited[i] = false;
+        for(int i = idx; i<chickens.size(); i++){
+            if(!chickenVisited[i]){
+                chickenVisited[i] = true;
+                dfs(cnt + 1, i + 1);
+                chickenVisited[i] = false;
             }
         }
     }
 
-    static class Point {
-        int x;
-        int y;
+    static class Node {
+        int i;
+        int j;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        public Node(int i, int j) {
+            this.i = i;
+            this.j = j;
         }
     }
 }
+
