b8ea7b33796b3eef8589f5600c4466b9ffe588ed
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-02-10 13:37:22 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,17 +1,5 @@
 def solution(str1, str2):
-    if str1 == str2:
+    if str2 in str1:
         return 1
-    elif len(str2) > len(str1):
+    else:
         return 2
-    
-    for i in range(0, len(str1) - len(str2) + 1):
-        flag = True
-        index = 0
-        for j in range(i, i + len(str2)):
-            if str1[j] != str2[index]:
-                flag = False
-            index += 1
-        if flag:
-            return 1
-                
-    return 2
