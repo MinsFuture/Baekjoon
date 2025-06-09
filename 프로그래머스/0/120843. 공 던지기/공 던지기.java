842f102924ea80f83b38dd1f567c2aac760a914e
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-06-09 15:14:18 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        
+        int index = 0;
+        for(int i = 0; i<k-1; i++){
+            index = (index + 2) % numbers.length;    
+        }
+        
+        
+        return numbers[index];
+    }
+}
