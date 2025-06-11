982ee662b4daab43cc868b83f93fca2d4277e506
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-06-11 13:46:27 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 666.98 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = []
+    
+    answer = myString.split('x')
+    while '' in answer:
+        answer.remove('')
+            
+    answer.sort()
+    
+    return answer
