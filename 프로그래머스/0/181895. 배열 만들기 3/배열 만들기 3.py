0d7bd8c5b449b3a0ea89cd5f944d9a6122e70d5e
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-06-11 13:39:24 +0900

    [level 0] Title: 배열 만들기 3, Time: 5.22 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -0,0 +1,11 @@
+def solution(arr, intervals):
+    answer = []
+    
+    for interval in intervals:
+        for i in range(interval[0], interval[1]+1):
+            answer.append(arr[i])
+    
+    
+    
+    
+    return answer
