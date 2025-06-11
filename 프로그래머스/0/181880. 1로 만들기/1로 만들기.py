7717f94fc4bebc155c03824d9b288196cac34740
이민혁 <104811969+MinsFuture@users.noreply.github.com>
2025-06-11 14:42:44 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    answer = 0
+    
+    for num in num_list:
+        cnt = 0
+        while num != 1:
+            if num % 2 == 0:
+                num = num / 2
+            else:
+                num = (num - 1) / 2
+            cnt += 1
+        answer += cnt
+        
+    return answer
